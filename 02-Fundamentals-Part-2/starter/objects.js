1b19f9b288eec37f3f8eb47e2de8da84a2876ade
ponjagannath <ponjagannath@gmail.com>
2022-12-07 22:07:53 +0530

    add

--- 02-Fundamentals-Part-2/starter/objects.js
+++ 02-Fundamentals-Part-2/starter/objects.js
@@ -1,11 +1,18 @@
 const me = {
   firstName: "Pon Jagannath",
   lastName: "Thangaswami",
+  birthYear: 1991
   age: 2037 - 1991,
   job: "Student",
   friends: ["Aadhitya", "Kavitha"],
+  hasDriversLicence: true,
+
+  calcAge: function (birthYear) {
+    return 2037 - birthYear;
+  },
 };
 
 console.log(me);
 console.log(me.firstName);
 console.log(me["firstName"]);
+console.log(me.calcAge(1991));
